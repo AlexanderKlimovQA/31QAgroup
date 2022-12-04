4083b77aeeb3083937d53a9985981db03c9985b9
eagleskyQA <eagle.skyqa@gmail.com>
2022-12-04 23:54:14 +0300

    added new tasks

--- SQL/StepikCourse/lesson3_1.sql
+++ SQL/StepikCourse/lesson3_1.sql
@@ -36,3 +36,43 @@ FROM student s
 GROUP BY name_student, name_subject
 HAVING COUNT(date_attempt) > 1
 ORDER BY Интервал;
+
+
+-- Студенты могут тестироваться по одной или нескольким дисциплинам (не обязательно по всем). Вывести дисциплину и количество уникальных студентов (столбец назвать Количество), которые по ней проходили тестирование . Информацию отсортировать сначала по убыванию количества, а потом по названию дисциплины. В результат включить и дисциплины, тестирование по которым студенты не проходили, в этом случае указать количество студентов 0.
+
+SELECT name_subject, COUNT(DISTINCT(student_id)) AS Количество
+FROM subject s
+     LEFT JOIN attempt a ON s.subject_id = a.subject_id
+GROUP BY name_subject
+ORDER BY Количество DESC, name_subject;
+
+
+-- Случайным образом отберите 3 вопроса по дисциплине «Основы баз данных». В результат включите столбцы question_id и name_question.
+
+SELECT question_id, name_question
+FROM subject s
+     JOIN question q ON s.subject_id = q.subject_id
+WHERE name_subject = 'Основы баз данных'
+ORDER BY RAND()
+LIMIT 3;
+
+
+-- Вывести вопросы, которые были включены в тест для Семенова Ивана по дисциплине «Основы SQL» 2020-05-17  (значение attempt_id для этой попытки равно 7). Указать, какой ответ дал студент и правильный он или нет(вывести Верно или Неверно). В результат включить вопрос, ответ и вычисляемый столбец  Результат.
+
+SELECT name_question, name_answer, IF(is_correct = 1, 'Верно', 'Неверно') AS Результат
+FROM answer a
+     JOIN testing t ON a.answer_id = t.answer_id
+     JOIN question q ON q.question_id = t.question_id
+WHERE attempt_id = 7;
+
+
+-- Посчитать результаты тестирования. Результат попытки вычислить как количество правильных ответов, деленное на 3 (количество вопросов в каждой попытке) и умноженное на 100. Результат округлить до двух знаков после запятой. Вывести фамилию студента, название предмета, дату и результат. Последний столбец назвать Результат. Информацию отсортировать сначала по фамилии студента, потом по убыванию даты попытки.
+
+SELECT name_student, name_subject, date_attempt, ROUND(SUM(is_correct)/3*100, 2) AS Результат
+FROM student s
+     LEFT JOIN attempt a ON s.student_id = a.student_id
+     JOIN subject su ON su.subject_id = a.subject_id
+     JOIN testing t ON t.attempt_id = a.attempt_id
+     JOIN answer an ON an.answer_id = t.answer_id
+GROUP BY name_student, name_subject, date_attempt
+ORDER BY name_student, date_attempt DESC;
