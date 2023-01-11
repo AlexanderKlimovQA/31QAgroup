a48dd30901b5cfa7b5c2fff1282510334abe01ea
eagleskyQA <eagle.skyqa@gmail.com>
2023-01-11 23:54:41 +0300

    added new SQL task

---
+++ SQL/StepikCourse_MySQL/lesson3_4.sql
@@ -0,0 +1,9 @@
+-- Создать вспомогательную таблицу applicant,  куда включить id образовательной программы,  id абитуриента, сумму баллов абитуриентов (столбец itog) в отсортированном сначала по id образовательной программы, а потом по убыванию суммы баллов виде (использовать запрос из предыдущего урока).
+
+CREATE TABLE applicant AS
+SELECT pe.program_id, es.enrollee_id, SUM(result) AS itog
+FROM program_enrollee pe JOIN
+    enrollee_subject es ON es.enrollee_id = pe.enrollee_id JOIN
+    program_subject ps ON ps.subject_id = es.subject_id AND ps.program_id = pe.program_id
+GROUP BY program_id, enrollee_id
+ORDER BY program_id, itog DESC;
