a1f87b067087fc0515812a7143e4773da129c2bf
eagleskyQA <eagle.skyqa@gmail.com>
2023-02-07 23:31:33 +0300

    added new SQL task

--- SQL/StepikCourse_MySQL/lesson3_4.sql
+++ SQL/StepikCourse_MySQL/lesson3_4.sql
@@ -62,3 +62,16 @@ UPDATE applicant_order
 SET str_id = IF(program_id = @pr_num, @row_num := @row_num + 1, @row_num := 1 AND @pr_num := @pr_num + 1);  -- Применение переменных для нумерации строк.
 
 SELECT * FROM applicant_order;
+
+
+-- Создать таблицу student,  в которую включить абитуриентов, которые могут быть рекомендованы к зачислению  в соответствии с планом набора. Информацию отсортировать сначала в алфавитном порядке по названию программ, а потом по убыванию итогового балла.
+
+CREATE TABLE student AS
+SELECT name_program, name_enrollee, itog
+FROM enrollee e JOIN
+     applicant_order ao ON e.enrollee_id = ao.enrollee_id JOIN
+     program p ON p.program_id = ao.program_id
+WHERE str_id <= plan
+ORDER BY name_program, itog DESC;
+
+SELECT * FROM student;
